383e3e60e721e49d2b3c18d8db158f12b62263eb
AlchemistPhantom <jerylenas@gmail.com>
2019-10-21 08:57:16 +0800

    membuat getCompany pada model company

--- src/models/companys.js
+++ src/models/companys.js
@@ -1,1 +1,15 @@
 const conn = require('../configs/db')
+
+module.exports={
+    getCompany: function(){
+        return new Promise(function(resolve,reject){
+            conn.query('SELECT * FROM tb_company',function(err,result){
+                if (!err) {
+                    resolve(result)
+                  } else {
+                    reject(new Error(err))
+                  }
+            })
+        })
+    }
+}

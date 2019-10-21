9ce4c097e13124a011e365ea55619f7ac0310011
AlchemistPhantom <jerylenas@gmail.com>
2019-10-21 09:01:54 +0800

    membuat getCompany pada controller company

---
+++ src/controllers/companys.js
@@ -0,0 +1,15 @@
+const companyModels = require('../models/companys')
+const uuid = require('uuid/v4')
+
+module.exports = {  
+    getCompany: function(req,res){
+        companyModels.getCompany()
+        .then(result=>{
+            res.json(result)
+        })
+        .catch(err=>{
+            console.log(err)
+        })
+    }
+    
+}
